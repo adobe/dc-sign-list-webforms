2f2c0adf77e460d874ca11c4b9570b4aeb7cf8be
Chris Hughes <cajhughes@gmail.com>
2023-10-13 09:24:12 +0100

    Include group name in output

--- src/main/java/uk/org/esig/adobe/widgets/ListWidgets.java
+++ src/main/java/uk/org/esig/adobe/widgets/ListWidgets.java
@@ -1,11 +1,14 @@
 package uk.org.esig.adobe.widgets;
 
 import io.swagger.client.api.BaseUrisApi;
+import io.swagger.client.api.GroupsApi;
 import io.swagger.client.api.UsersApi;
 import io.swagger.client.api.WidgetsApi;
 import io.swagger.client.model.ApiClient;
 import io.swagger.client.model.ApiException;
 import io.swagger.client.model.baseUris.BaseUriInfo;
+import io.swagger.client.model.groups.GroupInfo;
+import io.swagger.client.model.groups.GroupsInfo;
 import io.swagger.client.model.users.DetailedUserInfo;
 import io.swagger.client.model.users.UserInfo;
 import io.swagger.client.model.users.UsersInfo;
@@ -13,13 +16,16 @@ import io.swagger.client.model.widgets.UserWidget;
 import io.swagger.client.model.widgets.UserWidgets;
 import org.apache.commons.csv.CSVFormat;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ListWidgets {
     private static final String API_PATH = "api/rest/v6";
     private static final String API_URL = "https://api.adobesign.com/";
     private static final String API_USER_PREFIX = "email:";
     private static final String BEARER = "Bearer ";
+    private static final int CAPACITY = 20000;
     private static final int PAGE_SIZE = 1000;
     private static final String SANDBOX = "--sandbox";
     private static final String SANDBOX_API_URL = "https://api.adobesignsandbox.com/";
@@ -62,15 +68,37 @@ public class ListWidgets {
         /*
          *  Instantiate APIs for account
          */
+        GroupsApi groupsApi = new GroupsApi(apiClient);
         UsersApi usersApi = new UsersApi(apiClient);
         WidgetsApi widgetsApi = new WidgetsApi(apiClient);
+
+        Map<String, String> foundGroups = new HashMap<>(CAPACITY);
+
+        /*
+         *  Populate the list of groups
+         */
+        GroupsInfo groupsInfo = groupsApi.getGroups(accessToken, null, null, PAGE_SIZE);
+        List<GroupInfo> groupInfoList = groupsInfo.getGroupInfoList();
+        while (groupInfoList != null && !groupInfoList.isEmpty()) {
+            for (GroupInfo groupInfo: groupInfoList) {
+                foundGroups.put(groupInfo.getGroupId(), groupInfo.getGroupName());
+            }
+            String groupCursor = groupsInfo.getPage().getNextCursor();
+            if (groupCursor != null && !groupCursor.isEmpty()) {
+                groupsInfo = groupsApi.getGroups(accessToken, null, groupCursor, PAGE_SIZE);
+                groupInfoList = groupsInfo.getGroupInfoList();
+            }
+            else {
+                groupInfoList = null;
+            }
+        }
 
         /*
          *  Obtain the first page of users
          */
         UsersInfo usersInfo = usersApi.getUsers(accessToken, null, null, PAGE_SIZE);
         List<UserInfo> userInfoList = usersInfo.getUserInfoList();
-        System.out.println(format("webform_id", "webform_name", "owner_email"));
+        System.out.println(format("web_form_id", "web_form_name", "owner_email","group_name"));
         while (userInfoList != null && !userInfoList.isEmpty()) {
             /*
              *  For each user:
@@ -94,7 +122,8 @@ public class ListWidgets {
                         for (UserWidget widget : widgetList) {
                             System.out.println(format(widget.getId(),
                                                       widget.getName(),
-                                                      email));
+                                                      email,
+                                                      foundGroups.get(widget.getGroupId())));
                         }
                         String widgetCursor = widgets.getPage().getNextCursor();
                         if (widgetCursor != null && !widgetCursor.isEmpty()) {
@@ -122,8 +151,8 @@ public class ListWidgets {
         }
     }
 
-    private String format(String id, String name, String email) {
-        return CSVFormat.EXCEL.format(id, name, email);
+    private String format(String id, String name, String email, String groupName) {
+        return CSVFormat.EXCEL.format(id, name, email, groupName);
     }
 
     private static String getExceptionDetails(ApiException e) {
